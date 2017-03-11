2098b756a5d6314c915eba6a707699aac57b5104
ularbek <portmon6@gmail.com>
2017-03-11 15:38:57 +0600

    #4 add footer

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
 /**
- * Created by Актилек on 08.03.2017.
+ * Created by User on 11.02.2017.
  */
-$('.select-language select').change(function () {
-    $(this).parent().find('span').text($(this).find('option:selected').text());
-});
-$('.btn-select').click(function () {
-    $('#select-wrap').toggleClass('open');
-});
